4d83bb50252b9f95635225e104bd023809734dfb
bcliffordj <bizsolutionsbc@gmail.com>
2025-02-27 15:43:25 -0700

    Add App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BiomarkerTracker from './BiomarkerTracker';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BiomarkerTracker />
+    </div>
+  );
+}
+
+export default App;
